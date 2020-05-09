a757ebfc4f350b378f83edeaf726a5402065e58e
Mikko <mmnurm@gmail.com>
2020-05-09 18:02:02 +0300

    Corrected path for zsh plugins

--- .config/.bin/install.sh
+++ .config/.bin/install.sh
@@ -22,10 +22,9 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 echo "Getting Zsh plugins..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \ 
-	${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions \ 
-	${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+	~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 echo "Making zsh the default shell..."
 chsh -s $(which zsh)
