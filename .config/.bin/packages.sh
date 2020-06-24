8980d176e42a241b18e620d40c357687bef843bc
Mikko Nurminen <mmnurm@gmail.com>
2020-06-24 22:14:51 +0300

    minor changes

--- .config/.bin/packages.sh
+++ .config/.bin/packages.sh
@@ -15,7 +15,12 @@ sudo pacman -Sy --noconfirm urxvt-perls \
 	newsboat \
 	thunar \
 	urlscan \
-	fd
+	fd \
+	neovim
+
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # URxvt-perls
 yay -S urxvt-resize-font
